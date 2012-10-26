05a937877a024672d1264c58f1e2d2a944eb997e
cosmocoder <rodmguerra@gmail.com>
2012-10-25 22:57:43 -0200

    QueryDsl

---
+++ think-fast-game/src/main/java/br/com/senac/ccs/thinkfast/QuestionRepositoryImpl.java
@@ -0,0 +1,11 @@
+package br.com.senac.ccs.thinkfast;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rodrigo.mguerra
+ * Date: 25/10/12
+ * Time: 20:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class QuestionRepositoryImpl implements QuestionRepositoryExtension {
+}
